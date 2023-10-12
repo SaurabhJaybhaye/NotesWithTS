9a428ee11170d848fd14bbac65767a343f6ab9f1
Saurabh Jaybhaye <saurabh.jaybhaye@opcito.com>
2023-10-12 13:11:39 +0530

    Removed Unwanted files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,7 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import noteReducer from '../components/notes/noteSlice';
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     notes:noteReducer
   },
 });
